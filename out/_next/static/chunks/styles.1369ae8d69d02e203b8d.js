491b52376caf4d2818419eea9c9163f5e5417539
unknown <limteikyee@gmail.com>
2018-12-06 19:18:35 +0800

    housekeeping

--- out/_next/static/chunks/styles.1369ae8d69d02e203b8d.js
+++
@@ -1,4 +0,0 @@
-;(window.webpackJsonp = window.webpackJsonp || []).push([
-  [3],
-  { 1222: function(n, o, w) {}, 1328: function(n, o, w) {} },
-])
--- out/static/assets/jss/material-kit-pro-react/views/sectionsPageStyle.jsx
+++
@@ -1,9 +0,0 @@
-import { main } from '../../material-kit-pro-react.jsx'
-
-const sectionsPageStyle = {
-  main: {
-    ...main,
-  },
-}
-
-export default sectionsPageStyle
