5549183bd56ec0b9cf497b3b6df5b4cd08c5ce74
unknown <limteikyee@gmail.com>
2019-01-05 19:19:11 +0800

    imclude error handling for common error

--- src/utils/handleError.js
+++ src/utils/handleError.js
@@ -1,5 +1,11 @@
 const handleError = (error) => {
-  const err = JSON.parse(error.message.split('GraphQL error: ')[1])
+  let err = {}
+  try {
+    err = JSON.parse(error.message.split('GraphQL error: ')[1])
+  } catch (errs) {
+    err.statusCode = 0
+    err.message = error.message
+  }
   return {
     statusCode: err.statusCode,
     message: `${err.statusCode}: ${err.message}`,
