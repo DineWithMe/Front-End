165173172f3c01da568ffeb67c776c0c02275fc2
unknown <limteikyee@gmail.com>
2019-01-07 20:41:28 +0800

    reset state now come with setstate(plus delete cookis if on client side) and non setstate

--- src/utils/unstated.js
+++ src/utils/unstated.js
@@ -1,4 +1,7 @@
 import { Container } from 'unstated'
+// cookies
+import Cookies from 'js-cookie'
+import { USER_SESSION } from '../constants/cookies'
 
 const initialState = {
   login: false,
@@ -9,15 +12,20 @@ const initialState = {
 
 class UserStateContainer extends Container {
   state = initialState
-
+  setUserState = (state) => {
+    this.setState(state)
+  }
+  // these two methods doesn't need setState as they run before rendering
   initUserState = (state) => {
     this.state = state
   }
-  resetState = () => {
-    this.initUserState(initialState)
-  }
-  setUserState = (state) => {
-    this.setState(state)
+  resetUserState = (setState) => {
+    if (setState) {
+      this.setState(initialState)
+      process.browser && Cookies.remove(USER_SESSION)
+    } else {
+      this.initUserState(initialState)
+    }
   }
 }
 const userStateStore = new UserStateContainer()
