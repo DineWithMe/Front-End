9815bf71361df28611bbe222c809e15769fab4cc
unknown <limteikyee@gmail.com>
2019-01-08 00:01:07 +0800

    if already login, redirect to index page

--- pages/login.js
+++ pages/login.js
@@ -1,13 +1,21 @@
+import { Component } from 'react'
 import SignInPage from '../src/views/SignInPage/SignInPage'
 import Head from 'next/head'
+import redirectIndex from '../src/utils/redirectIndex.js'
 
-const SignIn = () => {
-  return (
-    <>
-      <Head />
-      <SignInPage />
-    </>
-  )
+class SignIn extends Component {
+  static async getInitialProps(ctx) {
+    await redirectIndex(ctx)
+    return {}
+  }
+  render() {
+    return (
+      <>
+        <Head />
+        <SignInPage />
+      </>
+    )
+  }
 }
 
 export default SignIn
