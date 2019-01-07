473f843af5509e19116f701d83c2a6821d24cd1b
unknown <limteikyee@gmail.com>
2019-01-08 00:34:01 +0800

    only user with their own token can view profile

--- pages/profile.js
+++ pages/profile.js
@@ -1,13 +1,22 @@
+import { Component } from 'react'
 import ProfilePage from '../src/views/ProfilePage/ProfilePage'
 import Head from 'next/head'
+import redirectIndex from '../src/utils/redirectIndex.js'
 
-const Profile = () => {
-  return (
-    <>
-      <Head />
-      <ProfilePage />
-    </>
-  )
+class Profile extends Component {
+  static async getInitialProps(ctx) {
+    await redirectIndex(ctx, true)
+    return {}
+  }
+
+  render() {
+    return (
+      <>
+        <Head />
+        <ProfilePage />
+      </>
+    )
+  }
 }
 
 export default Profile
