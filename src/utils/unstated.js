0be0f4680c5aceb921b8e12bbb170d0812ceac2b
unknown <limteikyee@gmail.com>
2019-01-08 19:18:03 +0800

    add new state propertise

--- src/utils/unstated.js
+++ src/utils/unstated.js
@@ -5,6 +5,7 @@ import { USER_SESSION } from '../constants/cookies'
 
 const initialState = {
   login: false,
+  userId: undefined,
   name: undefined,
   username: undefined,
   userToken: undefined,
