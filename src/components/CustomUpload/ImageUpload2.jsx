0ea7decbea1caec8e0143b1fe3d93f1477b5e80b
unknown <limteikyee@gmail.com>
2019-01-11 16:13:06 +0800

    working on image upload phase 1

--- src/components/CustomUpload/ImageUpload2.jsx
+++ src/components/CustomUpload/ImageUpload2.jsx
@@ -1,5 +1,9 @@
 import { Component } from 'react'
 import PropTypes from 'prop-types'
+// Apollo
+import { Mutation } from 'react-apollo'
+import { uploadFile } from '../../constants/mutationOperations'
+// material ui components
 import Tooltip from '@material-ui/core/Tooltip'
 // nodejs library that concatenates classes
 import classNames from 'classnames'
@@ -19,45 +23,61 @@ class ImageUpload2 extends Component {
       classes.imgFluid
     )
     return (
-      <>
-        <input
-          type='file'
-          ref={(c) => {
-            this.fileInput = c
-          }}
-          style={{ display: 'none' }}
-          onChange={(e) => {
-            const reader = new FileReader()
-            const file = e.target.files[0]
-            reader.onloadend = () => {
-              this.setState({
-                file,
-                imagePreviewUrl: reader.result,
-              })
-            }
-            reader.readAsDataURL(file)
-          }}
-          accept='image/png, image/jpeg'
-        />
-        <Tooltip
-          title='Change Your Avatar'
-          placement='right-end'
-          classes={{ tooltip: classes.tooltip }}
-        >
-          <img
-            src={imagePreviewUrl || christian}
-            alt='...'
-            className={imageClasses}
-            style={{
-              cursor: 'pointer',
-            }}
-            onClick={(e) => {
-              e.preventDefault()
-              this.fileInput.click()
-            }}
-          />
-        </Tooltip>
-      </>
+      <Mutation mutation={uploadFile}>
+        {(uploadFile, { data }) => (
+          <>
+            <input
+              type='file'
+              ref={(c) => {
+                this.fileInput = c
+              }}
+              style={{ display: 'none' }}
+              onChange={(e) => {
+                if (e.target.validity.valid) {
+                  const reader = new FileReader()
+                  const file = e.target.files[0]
+                  try {
+                    reader.readAsDataURL(file)
+                  } catch (err) {
+                    // this will be error if user cancel the to input any file
+                    // return and stop executing in case error
+
+                    return err
+                  }
+                  reader.onloadend = () => {
+                    this.setState({
+                      file,
+                      imagePreviewUrl: reader.result,
+                    })
+                  }
+                  uploadFile({ variables: { file } })
+
+                  console.log(reader.result)
+                }
+              }}
+              accept='image/png, image/jpeg'
+            />
+            <Tooltip
+              title='Change Your Avatar'
+              placement='right-end'
+              classes={{ tooltip: classes.tooltip }}
+            >
+              <img
+                src={imagePreviewUrl || christian}
+                alt='...'
+                className={imageClasses}
+                style={{
+                  cursor: 'pointer',
+                }}
+                onClick={(e) => {
+                  e.preventDefault()
+                  this.fileInput.click()
+                }}
+              />
+            </Tooltip>
+          </>
+        )}
+      </Mutation>
     )
   }
 }
